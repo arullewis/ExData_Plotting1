5b1cdfc87d6636938e98cf47738b29b0ac55e7c6
Arul Adaikkalam <arulraja@gmail.com>
2015-10-11 14:50:11 +0800

    Added comments

---
+++ plot2.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(lubridate)
+#create data directory
+
+if(!file.exists("data")){
+        dir.create("data")
+}
+
+#download & extract the file - first time only
+if(!file.exists("./data/housepowercon.zip"))
+{
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","./data/housepowercon.zip")
+unzip("./data/housepowercon.zip",exdir="./data?rea")
+        
+
+}
+
+if(!'powerdata'%in% ls())
+   {
+setAs("character","PDate", function(from) as.Date(from, format="%d/%m/%Y") )
+setClass("PDate")
+tableColClass<- c('PDate','character','numeric','numeric','numeric', 'numeric','numeric','numeric','numeric')
+
+powerdata<-read.csv("./data/household_power_consumption.txt", 
+                    colClasses = tableColClass,header=TRUE, sep=';' ,na.strings="?")
+
+powerdata<-powerdata[powerdata$Date>=as.Date("2007-02-01") & powerdata$Date<=as.Date("2007-02-02"),]
+
+
+
+}
+powerdata<-mutate(powerdata, obsdatetime = ymd_hms(paste(Date,Time)))
+
+
+png(filename="plot2.png", width=480, height=480, units="px")
+plot(powerdata$obsdatetime,powerdata$Global_active_power, type='l', xlab='', ylab='Global Active Power(kilowatts)')
+dev.off()
+
+
+          
+         
